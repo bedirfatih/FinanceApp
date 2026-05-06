4ad5f8fca842a509d1b9f16092afc4b34bbfb699
Fatih Bedir <bedirfatih418@gmail.com>
2026-05-06 17:46:57 +0300

    feat: backend fully tested and working

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+321df653a232ead916b4f4cd6b29549b3c3ba5f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d054c68fb2fa0d41f7ab587ced6257ecae120f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
